9a8fc1f40356d18bb7f0da6751371916d060889e
Tonnyjoh <ynnotjoh@gmail.com>
2024-10-19 20:19:14 +0300

    adding one page portfoliio

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,13 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
+        if (session()->get('isLoggedIn') && session()->get('role')=='client') {
+            return redirect()->to('/user/dashboard');
+        }elseif (session()->get('isLoggedIn') && session()->get('role')=='admin') {
+            return redirect()->to('/admin/dashboard');
+        }
         return view("index");
     }
 }
